fd2ef96ee91225576ba4dd74719c0b7fb7bf2207
chemeshuggah <chemeshuggah@gmail.com>
2022-08-23 17:11:23 +0200

    15

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
 st.sidebar.markdown("Headquarters")
     
@@ -31,23 +29,18 @@ add_selectbox = st.sidebar.selectbox(
 
 add_selectbox = st.sidebar.selectbox(
     "Group deck",
-    ("Email", "Home phone", "Mobile phone"))
-
+    ("Email", "Home phone", "Mobile phone")
+)
 
 add_selectbox = st.sidebar.selectbox(
     "Graveyard",
-    ("Email", "Home phone", "Mobile phone"))
+    ("Email", "Home phone", "Mobile phone")
+)
 
 if st.sidebar.button('Help'):
     st.sidebar.write('OK')
 
 st.header('Test')
-
-with st.container():
-    st.write("This is inside the container")
-
-    # You can call any Streamlit command, including custom components:
-    st.bar_chart(np.random.randn(50, 3))
 
 if st.button('Draw group card'):
     st.write('OK')
